591ebeadaf306cfa8c5ca84d522fc60f2160f3ae
bsuverzabaek <69542867+bsuverzabaek@users.noreply.github.com>
2021-06-01 23:21:24 -0600

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<Header />
+
+	<div>
+		<NameGame />
+	</div>
+</template>
+
+<script>
+	import Header from "./components/Header";
+	import NameGame from "./components/NameGame";
+
+	export default {
+		components: { Header, NameGame },
+	};
+</script>
