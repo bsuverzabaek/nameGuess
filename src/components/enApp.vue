fcdc2cb68de07d13f19bd30bbb95ec82f16705cd
bsuverzabaek <69542867+bsuverzabaek@users.noreply.github.com>
2021-06-22 22:52:28 -0600

    Add files via upload

---
+++ src/components/enApp.vue
@@ -0,0 +1,20 @@
+<template>
+	<Header />
+
+	<div>
+		<NameGame />
+	</div>
+</template>
+
+<script>
+	import Header from "../views/EN/Header";
+	import NameGame from "../views/EN/NameGame";
+
+	export default {
+		components: { Header, NameGame },
+	};
+</script>
+
+<style>
+	
+</style>
---
+++ src/components/jpApp.vue
@@ -0,0 +1,20 @@
+<template>
+	<HeaderJP />
+
+	<div>
+		<NameGameJP />
+	</div>
+</template>
+
+<script>
+	import HeaderJP from "../views/JP/HeaderJP";
+	import NameGameJP from "../views/JP/NameGameJP";
+
+	export default {
+		components: { HeaderJP, NameGameJP },
+	};
+</script>
+
+<style>
+	
+</style>
